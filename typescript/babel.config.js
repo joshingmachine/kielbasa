c5674af976663e526c0efadb22316f48ad105792
joshingmachine <josh@joshingmachine.com>
2019-10-18 21:52:08 -0400

    Converting files to TS

--- typescript/babel.config.js
+++ typescript/babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    plugins: [],
+    plugins: [
+        '@babel/proposal-class-properties',
+        '@babel/proposal-object-rest-spread',
+    ],
     presets: [
         [
+            '@babel/preset-typescript',
             '@babel/preset-env',
             {
                 targets: {
