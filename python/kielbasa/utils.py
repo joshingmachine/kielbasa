3074b96cf2b7ba70586e8fe5a9c4f97bdf11f17e
joshingmachine <josh@joshingmachine.com>
2019-11-05 20:07:34 -0500

    Working on Python utils

---
+++ python/kielbasa/utils.py
@@ -0,0 +1,10 @@
+def get_capitalized_word(word):
+    capitalized_word = word.capitalize()
+    return capitalized_word
+
+def getDelimiter(no_punctuation_string):
+    return ' '
+
+def getDashCase(original_string, get_modified_string, case_delimiter, options):
+    modified_string = get_modified_string(original_string)
+
